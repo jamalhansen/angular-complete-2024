5a5d1d92680448ddef718a75d646cfdb758171e6
Jamal Hansen <jamal.hansen@gmail.com>
2025-02-23 19:33:31 -0600

    Preparing for chapter 4

--- 03-modules/src/app/app.module.ts
+++ 03-modules/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { TasksComponent } from './tasks/tasks.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { AddTaskComponent } from './tasks/add-task/add-task.component';
 import { FormsModule } from '@angular/forms';
-import { CardComponent } from './shared/card/card.component';
+import { SharedModule } from './shared/card/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,12 +16,11 @@ import { CardComponent } from './shared/card/card.component';
     HeaderComponent,
     UserComponent,
     TasksComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     AddTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
